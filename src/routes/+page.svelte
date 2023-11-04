04eca975cce2a33bf20b88960779521d4bf21a2c
AN1001 <arnav.nagpure@icloud.com>
2023-11-04 18:37:03 +0000

    new landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
 	}
 </script>
 
+<h1>User State: <span style="color:{user.loggedIn?"yellow":"green"}">Logged {user.loggedIn?"in":"out"}</span></h1>
 {#if user.loggedIn}
 	<button on:click={toggle}> Log out </button>
 {/if}
@@ -13,3 +14,14 @@
 {#if !user.loggedIn}
 	<button on:click={toggle}> Log in </button>
 {/if}
+
+<style>
+    :root{
+        background: rgb(25,25,25);
+    }
+    
+    h1{
+        margin: 0;
+        font-family: sans-serif;
+    }
+</style>
