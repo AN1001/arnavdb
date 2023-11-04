435be2004fcc14e239ec9cd5d52a163140c760f3
AN1001 <arnav.nagpure@icloud.com>
2023-11-04 15:39:36 +0000

    g

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    let name = "Arnav";
+    let count = 0
+    function addCount(){
+        count+=1
+    }
+</script>
+
+<h1>Welcome to {name}'s website</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click={addCount}>
+    click me!
+</button>
+
+<div>
+    the count is {count}
+</div>
+
+<style>
+    div{
+        padding: 10px;
+        border-radius: 0.25em;
+        background: whitesmoke;
+        border: 2px solid yellowgreen;
+        width: 400px;
+        margin-top: 30px; 
+    }
+</style>
