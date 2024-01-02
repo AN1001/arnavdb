72adbcad3f4da66c0da368f12a920bdb398a163d
AN1001 <arnav.nagpure@icloud.com>
2024-01-02 21:24:24 +0530

    updates to user.js

--- user.js
+++ user.js
@@ -1,41 +1,46 @@
 async function postData(uid, sql) {
-    url = "https://arnavdb-manager.arnavium.workers.dev"
-    const response = await fetch(url, {
-      method: "GET",
-      mode: "cors", // no-cors, *cors, same-origin
-      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: "omit", // include, *same-origin, omit
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        "Content-Type": "application/json",
-        "sql":sql,
-        "uid":uid,
-      },
-      redirect: "follow", // manual, *follow, error
-      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    });
-    
-    response.text().then(function (text) {
-      console.log(text)
-    });
-}
+  url = "https://arnavdb-manager.arnavium.workers.dev"
+  const response = await fetch(url, {
+    method: "GET",
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "omit", // include, *same-origin, omit
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      "Content-Type": "application/json",
+      "sql":sql,
+      "uid":uid,
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+  });
   
+  response.text().then(function (text) {
+    console.log(text)
+  });
+}
+
 function runSQL(uid, sql){
-  	if(uid){
-    	sql = sql.split("FROM ").join(`FROM ${uid}_`)
-    } else {
-      throw Error("No uid specified on function 'runSQL'")
-    }
-  	console.log(sql)
-    postData(uid, sql)
+  if(uid){
+    sql = sql.split("FROM ").join(`FROM ${uid}_`)
+  } else {
+    throw Error("No uid specified on function 'runSQL'")
+  }
+  console.log(sql)
+  postData(uid, sql)
 }
 
 function append(){
-
+  
 }
 
-function get(uid, ){
-
+function get(uid, table, condition){
+  if(!uid){
+    throw Error("No uid specified on function 'get'")
+  }
+  const sql = `SELECT * FROM ${uid}_${table} WHERE ${condition}`
+  console.log(sql)
+  //postData(uid, sql)
 }
 
 function replace(){
@@ -47,23 +52,24 @@ function del(){
 }
 
 function createTable(uid, name, cols){
-    cols.forEach(function(col){
-        if(col.length>255){
-          throw Error(`Error - could not create table '${name}' as element '${col.slice(0,6)}...' exceeded limit of 255 chars`)
-        }
-    })
+  cols.forEach(function(col){
+      if(col.length>255){
+        throw Error(`Error - could not create table '${name}' as element '${col.slice(0,6)}...' exceeded limit of 255 chars`)
+      }
+  })
 
-    cols = cols.map(function(el){return el+" varChar(255)"})
-    sql = `CREATE TABLE ${uid}_${name} (${cols});`
-    console.log(sql)
-    postData(uid, sql)
+  cols = cols.map(function(el){return el+" varChar(255)"})
+  const sql = `CREATE TABLE ${uid}_${name} (${cols});`
+  console.log(sql)
+  postData(uid, sql)
 }	
 
 function DROPTABLE(){
 
 }
 
-runSQL("CONQ99", "SELECT * FROM NewTableTest1")
+get("CONQ99", "Persons", "LastName='Nagpure'")
+//runSQL("CONQ99", "SELECT * FROM NewTableTest1")
 //console.log(createTable("ss", "arnav", ["username", "passwordpasswor", "uid", "gid", "guuji-ness"]))
 //console.log(createTable("CONQ99", "NewTableTest1", ["username", "passwordpasswor", "uid", "gid", "guuji_ness"]))
 //UID of ArnavN is "CONQ99"
